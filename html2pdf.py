28f7a2361337798faca2f76aa396553ad2a94f33
Zach S <zsiegel92@gmail.com>
2018-05-31 14:30:18 -0700

    changes

---
+++ html2pdf.py
@@ -0,0 +1,16 @@
+import pdfkit
+
+css = 'style.css'
+options={
+	"margin-top":"0in",
+	"margin-bottom": "0in",
+	"margin-left": "0in",
+	"margin-right":"0in",
+	"page-height":"11in",
+	"page-width":"9in",
+	"encoding":"UTF-8",
+	"--no-print-media-type":""
+}
+pdfkit.from_file('index.html','index.pdf',options=options,css=css)
+
+# wkhtmltopdf --margin-bottom 0 --margin-top 0 --margin-left 0 --margin-right 0 --page-height 27.94 --page-width 24.13 --encoding UTF-8 --allow . --enable-plugins --no-print-media-type  index.html index.pdf
