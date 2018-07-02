373773a0251f8994eb7c59eb9e8fd2e26864497c
Zach S <zsiegel92@gmail.com>
2018-07-01 21:13:24 -0700

    resume edits

--- html2pdf.py
+++ html2pdf.py
@@ -9,8 +9,9 @@ options={
 	"page-height":"11in",
 	"page-width":"9in",
 	"encoding":"UTF-8",
-	"--no-print-media-type":""
+	"print-media-type":"",
+	"no-stop-slow-scripts":""
 }
-pdfkit.from_file('index.html','index.pdf',options=options,css=css)
-
+# pdfkit.from_file('index.html','index.pdf',options=options,css=css)
+pdfkit.from_file('index.html','index.pdf',options=options)
 # wkhtmltopdf --margin-bottom 0 --margin-top 0 --margin-left 0 --margin-right 0 --page-height 27.94 --page-width 24.13 --encoding UTF-8 --allow . --enable-plugins --no-print-media-type  index.html index.pdf
